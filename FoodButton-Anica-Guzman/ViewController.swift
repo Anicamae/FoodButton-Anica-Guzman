3b80dc7f9de8da0cce5a03cb05b51f49ea308e02
Anica Guzman <anicamaeg@gmail.com>
2020-09-07 19:47:42 -0700

    added images

--- FoodButton-Anica-Guzman/ViewController.swift
+++ FoodButton-Anica-Guzman/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func OrderPizzaPressed(_ sender: UIButton) {
+        messageLabel.text = "🍕 Price : $19.99"
+        imageView.image = UIImage(named: "image10")
+    }
+    
+    @IBAction func OrderBurgerPressed(_ sender: Any) {
+        messageLabel.text = "🍔 Price : $15.99"
+        imageView.image = UIImage(named: "image11")
+    }
+    
 }
 
