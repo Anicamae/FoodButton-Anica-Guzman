307e36bf93996cba8a09b8bdd511d0e366f3af62
Anica Guzman <anicamaeg@gmail.com>
2020-09-14 19:30:46 -0700

    loading new images

--- FoodButton-Anica-Guzman/ViewController.swift
+++ FoodButton-Anica-Guzman/ViewController.swift
@@ -9,26 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    
+var imageNumber = 0
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
         
     }
-
-    @IBAction func OrderPizzaPressed(_ sender: UIButton) {
-        messageLabel.text = "🍕 Price : $19.99"
-        imageView.image = UIImage(named: "pizza")
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        print(imageNumber)
+        // let imageName = "plant1" + String(imageNumber)
+        let imageName = "plant\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
+        
+        //        let awesomeMessage = "You Are Awesome!"
+        //        let greatMessage = "You Are Great!"
+        //        let bombMessage = "You Are Da Bomb!"
+        //
+        //        if messageLabel.text == awesomeMessage  {
+        //            messageLabel.text = greatMessage
+        //            imageView.image = UIImage(named: "plant1")
+        //        } else if messageLabel.text == greatMessage {
+        //            messageLabel.text = bombMessage
+        //            imageView.image = UIImage(named: "plant2")
+        //        } else  {
+        //            messageLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "plant3")
+        //
+        //        }
     }
     
-    @IBAction func OrderBurgerPressed(_ sender: Any) {
-        messageLabel.text = "🍔 Price : $15.99"
-        imageView.image = UIImage(named: "hamburger")
-    }
     
 }
 
